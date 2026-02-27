f72fa34f2cd72ab0de393ae1de2200405b19167e
baturinarmand <mandka1337@gmail.com>
2026-02-27 17:45:30 +0100

    a hibak kommentelese

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];        //NELEMENTS helyett N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa'      //sima '' helyett "", hianyzik a pontosvesszo 
+    for (int i = 0;)            //csak a kezdoertek van megadva a ciklusban
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)         //ha 0-rol indul a ciklus nem fog belepni ezert N_ELEMENTS>i kell 
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"           //nincs megadva az ertek amit ki akarunk iratni, hianyzik a pontosvesszo
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;                      //atlag nincs meghatarozva pl. int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS, i++)            //feltetelben vesszo helyett pontosvesszo kell
     {
-        atlag += b[i]
+        atlag += b[i]               //hianyzik a pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
