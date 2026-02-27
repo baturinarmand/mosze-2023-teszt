35406e90d84fa9f9eba7e661d47af07560caf7f9
baturinarmand <mandka1337@gmail.com>
2026-02-27 17:58:41 +0100

    hibak tenyleges javitasa

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];        //NELEMENTS helyett N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa'      //sima '' helyett "", hianyzik a pontosvesszo 
-    for (int i = 0;)            //csak a kezdoertek van megadva a ciklusban
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa";
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)         //ha 0-rol indul a ciklus nem fog belepni ezert N_ELEMENTS>i kell 
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"           //nincs megadva az ertek amit ki akarunk iratni, hianyzik a pontosvesszo
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;                      //atlag nincs meghatarozva pl. int atlag = 0;
-    for (int i = 0; i < N_ELEMENTS, i++)            //feltetelben vesszo helyett pontosvesszo kell
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]               //hianyzik a pontosvesszo
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
+
